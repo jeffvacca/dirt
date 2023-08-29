a5d08deed6533887c3ec7c169d480ea51699e7f4
Jeff Vacca <jeffvacca@gmail.com>
2023-08-29 16:57:43 -0400

    test

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: './dirt',
+  base: '/dirt',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
