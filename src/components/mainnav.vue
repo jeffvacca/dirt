12d081791c4ebc49ecc110b2a0fa01e598300a68
Jeff Vacca <jeffvacca@gmail.com>
2023-10-11 15:02:27 -0400

    add socials, azzuro ent image

--- src/components/mainnav.vue
+++ src/components/mainnav.vue
@@ -45,7 +45,11 @@ export default {
             <template #start>
                 <img alt="logo" src="/img/dirt-painting-sm.jpg" height="40" class="mr-2" />
             </template>
-
+            <template #end>
+                <a href="https://www.facebook.com/profile.php?id=61552021618031" target="_blank" class="text-color mx-2"><i class="pi pi-facebook text-2xl"></i></a>
+                <a href="https://www.instagram.com/dirtgrungetribute/" target="_blank" class="text-color mx-2"><i class="pi pi-instagram text-2xl"></i>
+                </a>
+            </template>
         </Menubar>
     </div>
 </template>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -23,16 +23,30 @@ export default {
         <h4>90's Grunge Tribute</h4>
       </div>
       <div class="col-12 lg:col-8 lg:col-offset-2 xl:col-6 xl:col-offset-3 px-2 mb-6">
-        <Card>
+        <Card class="mb-8">
           <template #title> Booking Inquiries </template>
           <template #content>
-              <dl>
-                <dt class="text-lg font-bold">Azzurro Entertainment</dt>
-                <dd class="mx-0 my-2">561-315-1985</dd>
-                <dd class="mx-0 my-2"><a href="mailto:Azzurroentertainment@gmail.com">Azzurroentertainment@gmail.com</a></dd>
-              </dl>
+            <div class="flex align-items-center justify-content-center">
+              <img class="w-8 md:w-6" src="/img/azzuro-ent.jpg" alt="Azzuro Entertainment">
+
+            </div>
+            <dl>
+              <dt class="text-lg font-bold">Azzurro Entertainment</dt>
+              <dd class="mx-0 my-2">561-315-1985</dd>
+              <dd class="mx-0 my-2"><a href="mailto:Azzurroentertainment@gmail.com">Azzurroentertainment@gmail.com</a>
+              </dd>
+            </dl>
           </template>
         </Card>
+
+
+        <div class="flex align-items-center justify-content-center">
+          <a href="https://www.facebook.com/profile.php?id=61552021618031" target="_blank" class="text-color mx-4"><i
+              class="pi pi-facebook text-4xl"></i></a>
+          <a href="https://www.instagram.com/dirtgrungetribute/" target="_blank" class="text-color mx-4"><i
+              class="pi pi-instagram text-4xl"></i>
+          </a>
+        </div>
       </div>
     </div>
 
